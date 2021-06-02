977ab031cc3c7ba6ef219c0322ecf17c56c6d953
Svar9 <tolik-pal@yandex.ru>
2021-06-02 18:02:05 +0300

    header almost done

---
+++ #src/js/files/script.js
@@ -0,0 +1,6 @@
+
+let icon = document.querySelector(".user-header__icon");
+icon.addEventListener('Click', function(e){
+   // document.querySelector(".user-header__menu");
+    console.log('1');
+});
