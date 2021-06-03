2fd297262141d2413fe068af7c410b46c740d51d
Svar9 <tolik-pal@yandex.ru>
2021-06-03 12:15:42 +0300

    finished header

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,6 +1,12 @@
 
 let user_icon = document.querySelector(".user-header__icon");
+let user_menu = document.querySelector(".user-header__menu");
 user_icon.addEventListener('click', function(e){
-   let user_menu = document.querySelector(".user-header__menu");
    user_menu.classList.toggle("_active");
 });
+
+document.addEventListener('click',function(e){
+   if(!e.target.closest('.user-header')){
+      user_menu.classList.remove("_active");
+   }
+}); 
