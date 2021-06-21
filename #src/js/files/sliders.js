1fbd975c78705204ace3a0f3fd60fb25010883ac
Svar9 <tolik-pal@yandex.ru>
2021-06-21 17:18:20 +0300

    continiue

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -110,3 +110,53 @@ let main_slider = new Swiper('.main-slider__body', {
 	//	el: '.swiper-scrollbar',
 	//},
 });
+
+let main_slider = new Swiper('.slider-lots__body', {
+	/*
+	effect: 'fade',
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	*/
+	observer: true,
+	observeParents: true,
+	slidesPerView: 3,
+	spaceBetween: 0,
+	//autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	//preloadImages: false,
+	//lazy: true,
+	// Dotts
+	//pagination: {
+	//	el: '.slider-quality__pagging',
+	//	clickable: true,
+	//},
+	// Arrows
+	navigation: {
+		nextEl: '.control-main-slider__arrow_next',
+		prevEl: '.control-main-slider__arrow_prev',
+	},
+	
+	/* breakpoints: {
+		320: {
+			autoHeight: true,
+		},
+		768: {
+			autoHeight:false,
+		}
+	}, */
+	
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	//scrollbar: {
+	//	el: '.swiper-scrollbar',
+	//},
+});
